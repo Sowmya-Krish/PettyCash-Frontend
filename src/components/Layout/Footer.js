37697969b484455ccd8e7c81f72886754c9f3f6b
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2023-04-18 06:03:00 +0530

    PettyCash-Frontend

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark t p-4">
+      <h6 className="text-center text-white">
+        {"\u2728"} PettyCash App created by Sowmya {"\u2728"}
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
